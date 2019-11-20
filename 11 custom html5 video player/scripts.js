4c64fb2d1f57ee7c3978c61071d680309734b924
DianaYJC <51901766+DianaYJC@users.noreply.github.com>
2019-11-20 14:20:08 -0800

    html finished

---
+++ 11 custom html5 video player/scripts.js
@@ -0,0 +1,34 @@
+const player=document.querySelector('.player')
+const video=player.querySelector('.viewer')
+const progress=player.querySelector('.progress')
+const progressBar=player.querySelector('.progress__filled')
+const toggle=player.querySelector('.toggle')
+const skipButtons=player.querySelectorAll('[data-skip]')
+const ranges = player.querySelectorAll('.player__slider');
+
+// Build function
+function togglePlay(){
+    // if(video.paused){
+    //     video.play()
+    // }else{
+    //     video.pause()
+    // }
+    video[video.paused? 'play':'pause']()
+}
+function updateButton(){
+    toggle.textContent=this.paused? '►' : '❚ ❚';
+}
+function skip(){
+    video.currentTime+=parseFloat(this.dataset.skip)
+}
+function handleRangeUpdate(){
+    video[this.name]=this.value
+}
+// hook with eventlisner
+video.addEventListener('click', togglePlay)
+video.addEventListener('play',updateButton)
+video.addEventListener('pause',updateButton)
+toggle.addEventListener('click',togglePlay)
+skipButtons.forEach(button => button.addEventListener('click',skip))
+ranges.forEach(range=>range.addEventListener('change',handleRangeUpdate))
+ranges.forEach(range=>range.addEventListener('mousemove',handleRangeUpdate))
